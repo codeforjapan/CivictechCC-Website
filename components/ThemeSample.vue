8ea926ee09709a2a44384d7de37a2a41fe8e3a9a
Nekoya3 <taiyu31@gmail.com>
2020-07-13 15:10:24 +0900

    :lipstick: Add Other Theme Description

--- components/ThemeSample.vue
+++ components/ThemeSample.vue
@@ -1,10 +1,25 @@
 <template>
   <section class="ThemeSample">
-    <section-title
-      class="ThemeSample-Title"
-      en-title="Theme Sample"
-      ja-title="テーマサンプル"
-    />
+    <v-row>
+      <v-col cols="12" md="6">
+        <section-title
+          class="ThemeSample-Title"
+          en-title="Theme Sample"
+          ja-title="テーマサンプル"
+        />
+      </v-col>
+      <v-col>
+        <div class="ThemeSample-Description">
+          <span>
+            テーマは何でもOK！
+          </span>
+          <br />
+          <span>
+            自分が興味のあるテーマを設定しよう！
+          </span>
+        </div>
+      </v-col>
+    </v-row>
     <v-flex grow wrap>
       <v-row wrap>
         <v-col cols="12" md="3" sm="6">
@@ -29,6 +44,16 @@
   &-Title {
     margin-left: 0;
     margin-right: auto;
+  }
+  &-Description {
+    font-family: 'Noto Sans JP', sans-serif;
+    font-size: 30px;
+    font-weight: 700;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.5;
+    letter-spacing: normal;
+    color: #170f7b;
   }
 }
 </style>
