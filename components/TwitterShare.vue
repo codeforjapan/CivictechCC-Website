f856236b779a1cb0e4b6d58fdcc35ea8b638538d
Yuto Ashida <34832037+y-chan@users.noreply.github.com>
2020-07-30 22:10:09 +0900

    Update components/TwitterShare.vue
    Co-authored-by: MaySoMusician <maysomusician@gmail.com>

--- components/TwitterShare.vue
+++ components/TwitterShare.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="TwitterShare">
-    <v-btn rounded color="#00acee" class="ShareButton" @click="twitter">
+    <v-btn
+      rounded
+      color="#1DA1F2"
+      class="ShareButton"
+      href="https://twitter.com/intent/tweet?text=Civictech%20Challenge%20Cup%20U-22&url=https://ccc2020.code4japan.org&hashtags=CCCu22"
+      target="noopener noreferrer"
+    >
       <img src="/twitter.svg" width="20" alt="twitter" />
       <span>Twitterでシェア</span>
     </v-btn>
