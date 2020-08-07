e88a7b4a742706d56af0cb527ba73975e9072cd1
Nekoya3 <taiyu31@gmail.com>
2020-08-07 22:41:01 +0900

    :lipstick: fix twitterbutton designs

--- components/TwitterShare.vue
+++ components/TwitterShare.vue
@@ -1,29 +1,25 @@
 <template>
-  <v-row class="TwitterShare">
-    <v-col cols="6" class="ButtonCols">
-      <v-btn
-        fab
-        small
-        elevation="1"
-        color="#1DA1F2"
-        class="ShareButton"
-        href="https://twitter.com/intent/tweet?text=Civictech%20Challenge%20Cup%20U-22&url=https://ccc2020.code4japan.org&hashtags=CCCu22"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src="/twitter.svg" width="20" alt="twitter" />
-      </v-btn>
-    </v-col>
-    <v-col cols="6" class="HashtagCols">
-      <a
-        href="https://twitter.com/intent/tweet?text=Civictech%20Challenge%20Cup%20U-22&url=https://ccc2020.code4japan.org&hashtags=CCCu22"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        #CCCu22 でシェア
-      </a>
-    </v-col>
-  </v-row>
+  <div class="ButtonCols">
+    <v-btn
+      fab
+      small
+      elevation="1"
+      color="#1DA1F2"
+      class="ShareButton"
+      href="https://twitter.com/intent/tweet?text=Civictech%20Challenge%20Cup%20U-22&url=https://ccc2020.code4japan.org&hashtags=CCCu22"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="/twitter.svg" width="20" alt="twitter" />
+    </v-btn>
+    <a
+      href="https://twitter.com/intent/tweet?text=Civictech%20Challenge%20Cup%20U-22&url=https://ccc2020.code4japan.org&hashtags=CCCu22"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      #CCCu22 でシェア
+    </a>
+  </div>
 </template>
 
 <script>
@@ -33,34 +29,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.TwitterShare {
+.ButtonCols {
   font-family: $font-ja;
   font-size: 20px;
   font-weight: bold;
   color: #170f7b;
+  margin: 0 auto;
+  text-align: left;
+  width: 240px;
+  .ShareButton {
+    text-transform: none;
+    font-size: 15px;
+    font-weight: bold;
+    color: #fff;
+    margin: 0 auto;
 
-  .ButtonCols {
-    text-align: right;
-    .ShareButton {
-      text-transform: none;
-      font-size: 15px;
-      font-weight: bold;
-      color: #fff;
-
-      .TwitterIcon {
-        width: 25px;
-        height: 25px;
-        margin: 10px 0;
-      }
+    .TwitterIcon {
+      width: 25px;
+      height: 25px;
+      margin: 10px 0;
     }
   }
-  .HashtagCols {
-    margin: auto 0;
-    text-align: left;
-    a {
-      color: #170f7b;
-      text-decoration: none;
-    }
+
+  a {
+    color: #170f7b;
+    text-decoration: none;
+    margin: 0 10px;
   }
 }
 </style>
