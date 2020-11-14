3673f8631b67c18302bf9ae22b094508f7a98fc3
Kota Yatagai <kota@yatagai.com>
2020-11-14 13:30:05 +0900

    ビュレット削除と大賞の画像追加、企業賞を横並びに変更

--- components/Result.vue
+++ components/Result.vue
@@ -3,12 +3,15 @@
     <section-title class="Result-Title" en-title="Result" ja-title="結果" />
     <div class="Result-Description">
       <ul>
-        <li>
-          <span class="FirstPrize">
-            <div class="FirstPrize-Award">大賞 :</div>
-            <div class="FirstPrize-Team">Civichat</div>
-          </span>
-        </li>
+        <div>
+          <li>
+            <div class="FirstPrize">
+              <div class="FirstPrize-Award">大賞 :</div>
+              <div class="FirstPrize-Team">Civichat</div>
+              <div class="FirstPrize-Img"><img src="@/static/champ.png" /></div>
+            </div>
+          </li>
+        </div>
         <li v-for="(award, index) in award_result" :key="index">
           <div class="Result-Description-Award">{{ award.award_name }} :</div>
           <div class="Result-Description-Team">{{ award.team_name }}</div>
@@ -42,7 +45,6 @@ export default {
 <style lang="scss" scoped>
 .Result {
   margin-bottom: 50px;
-
   &-Title {
     margin: {
       left: 0;
@@ -73,16 +75,19 @@ export default {
     }
 
     li {
+      display: inline-block;
+      list-style: none;
+      margin-left: -1rem;
       line-height: 20px;
-      padding: 1em;
-      padding-left: 0.5em;
-      .Result-Description-Award {
-        font-size: 15px;
-        margin-bottom: 10px;
-      }
-      .Result-Description-Team {
-        font-size: 30px;
-      }
+      padding: 1em 2em;
+    }
+    &-Award {
+      font-size: 15px;
+      margin-bottom: 10px;
+    }
+    &-Team {
+      font-size: 35px;
+      font-weight: bold;
     }
 
     &-SeeAlso {
@@ -95,9 +100,38 @@ export default {
   }
 }
 .FirstPrize {
+  width: 100%;
   line-height: 50px;
   &-Team {
     font-size: 50px;
+  }
+  img {
+    margin-top: 1em;
+    width: 50vw;
+  }
+}
+
+@media screen and (max-width: 750px) {
+  .Result {
+    &-Description {
+      ul {
+        padding-left: 24px;
+      }
+      li {
+        display: block;
+        padding: 1em 0;
+      }
+    }
+    .FirstPrize {
+      &-Img {
+        margin: 0 auto;
+        margin-left: -24px; //ulのpadding-left分
+        text-align: center;
+        img {
+          width: 80vw;
+        }
+      }
+    }
   }
 }
 </style>
